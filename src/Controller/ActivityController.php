76009ea62a100d0240bbafbd49b6de3f91cb3db0
Rania <raniaattar1616@gmail.com>
2025-05-06 16:20:37 +0200

    correction class

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ActivityApiController extends AbstractController
+final class ActivityController extends AbstractController
 {
     #[Route('/activity/api', name: 'app_activity_api')]
     public function index(): Response
